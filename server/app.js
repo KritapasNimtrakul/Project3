573b6c866defb819cc91b7063f5c3915bd0556b6
KritapasNimtrakul <kxn9945@rit.edu>
2019-03-11 21:24:29 -0400

    fix favicon

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(favicon(`${__dirname}/../hosted/img/favicon-32x32.png`));
 app.use(compression());
 app.disable('x-powered-by');
 app.use(bodyParser.urlencoded({
